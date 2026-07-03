5f26db4de4579a9058055c400868e4a032920989
devashish0407 <tech.devashish07@gmail.com>
2026-07-03 17:15:54 +0530

    feat: add static branding assets and initial landing page template

--- src/traffic_sign_system/interfaces/web/blueprints/views.py
+++ src/traffic_sign_system/interfaces/web/blueprints/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, Response, send_from_directory, current_app
+import os
 
 views_bp = Blueprint("views", __name__)
 
@@ -9,7 +10,7 @@ def index():
 @views_bp.route("/favicon.ico")
 def favicon():
     return send_from_directory(
-        current_app.static_folder,
+        os.path.join(current_app.static_folder, "logo"),
         "favicon.ico",
         mimetype="image/vnd.microsoft.icon"
     )
